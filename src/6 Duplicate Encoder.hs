a76901cd5b910fe546b3fdb4124aa49b8b4fd9aa
shinnku <nikaidou@shinnku.com>
2025-06-11 16:23:29 +0800

    save

--- src/6 Duplicate Encoder.hs
+++ src/6 Duplicate Encoder.hs
@@ -1,4 +1,10 @@
 module Dups where
 
+import Data.Char (toLower)
+
 duplicateEncode :: String -> String
-duplicateEncode = error "y no codez?"
+duplicateEncode s = map (\c -> if countChar c ls > 1 then ')' else '(') ls
+  where
+    ls = map toLower s
+    countChar :: Char -> String -> Int
+    countChar c = length . filter (== c)
